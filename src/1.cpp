0aef12a464d801d2268bf92021a19ca6ff3c39c4
huangjx <huangjx@shukun.net>
2024-05-31 17:48:42 +0800

    fix: reference and pointer

--- src/1.cpp
+++ src/1.cpp
@@ -13,54 +13,58 @@ public:
     std::string m_name;
 };
 
-void print(const Person* person)
+std::ostream& print(std::ostream& os, const Person* person)
 {
     // 因为指针可以为NULL，所以使用指针需要判定是不是NULL
     if (person)
     {
-        std::cout << "Person[" << person->m_name << "]" << std::endl;
+        return os << "Person[" << person->m_name << "]" << std::endl;
     }
     else
     {
-        std::cout << "Person == NULL" << std::endl;
+        return os << "Person == NULL" << std::endl;
     }
 }
 
-void print(const Person& person)
+std::ostream& print(std::ostream& os, const Person& person)
 {
-    // 因为引用不可以为NULL，所以使用引用不需要判断NULL
-    std::cout << "Person(" << person.m_name << ")" << std::endl;
+    // 因为引用不可以为NULL，所以使用引用不需要判断NULL 
+    return os << "Person(" << person.m_name << ")" << std::endl;
 }
 
 
 int main()
 {
-    Person person1("Hello");
+    Person person1("More");
 
-    Person person2("World");
+    Person person2("Effective");
 
-    Person person3("XXXXXX");
+    Person person3("C++");
 
     //Person& ref1;     // ERROR: 因为引用不可以为NULL，所以必须有初值
 
-    Person& ref1 = person1; //OK 有初值
-    print(ref1);        // ref1 = Hello
-    print(person1);     // person1 = Hello
+    Person& ref1 = person1;    //OK 有初值
+    print(std::cout << "init! ref1 reference person1: ref1 = ", ref1);        // ref1 = More
+    print(std::cout << "init! ref1 reference person1: person1 = ", person1);  // person1 = More
 
     person1 = person2;
-    print(ref1);       // ref1 = World
-    print(person1);    // person1 = World
+    print(std::cout << "ref1 reference person1, but person2 assign to person1, ref1 = ", ref1);      // ref1 = Effective
+    print(std::cout << "ref1 reference person1, but person2 assign to person1, person1 = ", person1);   // person1 = Effective
 
     ref1 = person3;   // 无法改为对person3的引用，是把person3的值赋值给person1
-    print(ref1);      // ref1 = XXXXXX
-    print(person1);   // person1 = XXXXXX
+    print(std::cout << "ref1 reference person1, but person3 assign to ref1, ref1 = ", ref1);         // ref1 = C++
+    print(std::cout << "ref1 reference person1, but person3 assign to ref1, person1 = ", person1);   // person1 = C++
 
     std::cout << "==========指针可以改变指向==========" << std::endl;
     Person* person4 = &person1;
-    print(person4);   // person4 = Hello
+    print(std::cout << "person4 point to person1, person4 = ", person4);   // person4 -> person1
 
     person4 = &person2;
-    print(person4);   // person4 = World
+    print(std::cout << "person4 point to person2, person4 = ", person4);   // person4 -> person2
+
+    Person* p = NULL;
+    Person& pref = *p;
+    print(std::cout << "no no no", pref);
 
     return 0;
 }
